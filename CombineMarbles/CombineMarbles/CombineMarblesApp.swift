505852fe36ee9a70cdf3ccfc97964abb10f26695
Ilya Maslau <imaslov@gmail.com>
2022-04-27 18:01:09 +0300

    presetting project

---
+++ CombineMarbles/CombineMarbles/CombineMarblesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineMarblesApp.swift
+//  CombineMarbles
+//
+//  Created by Ilya Maslau on 27.04.22.
+//
+
+import SwiftUI
+
+@main
+struct CombineMarblesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineMarbles/CombineMarbles/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineMarbles
+//
+//  Created by Ilya Maslau on 27.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
